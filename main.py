bb17f0903c93a527c5b58ffcc2cb61f600c31ad1
Dodger23 <tarekhassan3874@gmail.com>
2021-02-07 21:04:52 +0200

    changed path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, render_template
 
 
-app = Flask(__name__ , static_url_path='static')
+app = Flask(__name__ , static_url_path='/static')
 
 
 @app.route("/")
