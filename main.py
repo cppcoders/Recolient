28e1a3aa959049f672623a09d6ba05126549813c
sudofix <mo7amed.ram@gmail.com>
2021-02-08 03:18:26 +0200

    added voice recording

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
+import wave
 from flask import Flask, request, redirect, render_template
 
 
-app = Flask(__name__ )
+app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return render_template('home.html')
 
+
 @app.route("/clients")
 def mod():
     return render_template('clients.html')
+
 
 @app.route("/more")
 def about():
     return render_template('more.html')
 
-        
+
+@app.route("/Afile", methods=['POST'])
+def afile():
+    blob = request.files['file'].read()
+    with open('myfile.wav', mode='wb') as f:
+        f.write(blob)
+    from flask import jsonify
+    resp = jsonify(success=True)
+    return resp
+
+
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
