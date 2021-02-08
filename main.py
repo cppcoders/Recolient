fe4cc764f52d750a6ee448da21074304c61441a4
sudofix <mo7amed.ram@gmail.com>
2021-02-08 03:45:10 +0200

    disabled caching

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def afile():
     return resp
 
 
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-
 if __name__ == '__main__':
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
     app.run()
