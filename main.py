bd96f1c5cf4ec754d90ec1b147761260c80c3ee4
sudofix <mo7amed.ram@gmail.com>
2021-02-12 18:22:02 +0200

    connected with server

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import wave
 from flask import Flask, request, redirect, render_template
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
@@ -20,30 +22,8 @@ def about():
     return render_template('more.html')
 
 
-@app.route("/getfile")
-def getfile():
-    from flask import send_file
-    path_to_file = "myfile.wav"
-
-    return send_file(
-        path_to_file,
-        mimetype="audio/wav",
-        as_attachment=True,
-        attachment_filename="test.wav")
-
-
-@app.route("/Afile", methods=['POST'])
-def afile():
-    blob = request.files['file'].read()
-    with open('myfile.wav', mode='wb') as f:
-        f.write(blob)
-    from flask import jsonify
-    resp = jsonify(success=True)
-    return resp
-
-
 if __name__ == '__main__':
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-    app.run()
+    app.run(port=5200)
